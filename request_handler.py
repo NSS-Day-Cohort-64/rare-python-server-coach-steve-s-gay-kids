4a01ec46ff264698c7dc6635049182398172a12a
Briana Phillips <briana.eden.phillips@gmail.com>
2023-08-14 09:26:42 -0500

    posts displaying

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
-from views.user import create_user, login_user
+from views.user_requests import create_user, login_user
 from views.tags_request import get_all_tags, get_single_tag, create_tag, delete_tag, update_tag
 from views.posts_requests import get_all_posts, get_single_post
 
@@ -43,8 +43,10 @@ class HandleRequests(BaseHTTPRequestHandler):
         """Sets the OPTIONS headers"""
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
     def do_GET(self):
@@ -53,18 +55,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         response = {}
 
         (resource, id) = self.parse_url(self.path)
-        if resource == "tags":
-            if id is not None:
-                response = get_single_tag(id)
-                success = True
-            else:
-                response = get_all_tags()
-                success = True
-        if success:
-            self._set_headers(200)
-        else:
-            self._set_headers(404)
-            
+
         if resource == "posts":
             if id is not None:
                 response = get_single_post(id)
@@ -83,6 +74,13 @@ class HandleRequests(BaseHTTPRequestHandler):
             else:
                 response = {'error': 'User not found'}
 
+        if resource == "tags":
+            if id is not None:
+                response = get_single_tag(id)
+
+            else:
+                response = get_all_tags()
+
         # Convert the response to a JSON string and send it as the response body
 
         self.wfile.write(json.dumps(response).encode())
@@ -92,7 +90,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         content_len = int(self.headers.get('content-length', 0))
         post_body = json.loads(self.rfile.read(content_len).decode())
 
-        resource = self.parse_url()[0]  # Access the 'resource' from the 'parse_url' method
+        # Access the 'resource' from the 'parse_url' method
+        resource = self.parse_url()[0]
         response = ""
 
         if resource == 'login':
@@ -110,7 +109,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         post_body = self.rfile.read(content_len)
         post_body = json.loads(post_body)
 
-        resource, id = self.parse_url()  # Assign the result of 'self.parse_url()' to 'resource' and 'id'
+        # Assign the result of 'self.parse_url()' to 'resource' and 'id'
+        resource, id = self.parse_url()
 
         success = False
         error = ""
--- views/posts_requests.py
+++ views/posts_requests.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Posts
+from models import Posts, Users, Categories
 
 
 def get_all_posts():
@@ -13,15 +13,29 @@ def get_all_posts():
         # Write the SQL query to get the information you want
         db_cursor.execute("""
             SELECT
-                id,
-                user_id,
-                category_id,
-                title,
-                publication_date,
-                image_url,
-                content,
-                approved             
-            FROM Posts
+                p.id,
+                p.user_id,
+                p.category_id,
+                p.title,
+                p.publication_date,
+                p.image_url,
+                p.content,
+                p.approved,
+                u.first_name first_name,
+                u.last_name last_name,
+                u.email email,
+                u.bio bio,
+                u.username username,
+                u.password password,
+                u.profile_image_url image,
+                u.created_on date,
+                u.active active,        
+                c.label category             
+            FROM `Posts` p
+            JOIN Users u 
+                ON u.id = p.user_id
+            JOIN Categories c
+                ON c.id = p.category_id
             """)
 
         # Initialize an empty list to hold all order representations
@@ -33,11 +47,19 @@ def get_all_posts():
         # Iterate list of data returned from database
         for row in dataset:
 
-            # Create an animal instance from the current row
             post = Posts(row['id'], row['user_id'],
                          row['category_id'], row['title'],
                          row['publication_date'], row['image_url'],
                          row['content'], row['approved'])
+
+            user = Users(row['id'], row['first_name'], row['last_name'], row['email'], row['bio'],
+                         row['username'], row['password'], row['image'], row['date'], row['active'])
+
+            categories = Categories(row['id'], row['category'])
+
+            post.user = user.__dict__
+
+            post.categories = categories.__dict__
 
             posts.append(post.__dict__)
 
@@ -53,15 +75,29 @@ def get_single_post(id):
         # into the SQL statement.
         db_cursor.execute("""
             SELECT
-                id,
-                user_id,
-                category_id,
-                title,
-                publication_date,
-                image_url,
-                content,
-                approved             
-            FROM Posts
+                p.id,
+                p.user_id,
+                p.category_id,
+                p.title,
+                p.publication_date,
+                p.image_url,
+                p.content,
+                p.approved,
+                u.first_name first_name,
+                u.last_name last_name,
+                u.email email,
+                u.bio bio,
+                u.username username,
+                u.password password,
+                u.profile_image_url image,
+                u.created_on date,
+                u.active active,        
+                c.label category             
+            FROM `Posts` p
+            JOIN Users u 
+                ON u.id = p.user_id
+            JOIN Categories c
+                ON c.id = p.category_id
             WHERE id = ?
             """, (id, ))
 
@@ -73,5 +109,14 @@ def get_single_post(id):
                      data['category_id'], data['title'],
                      data['publication_date'], data['image_url'],
                      data['content'], data['approved'])
+
+        user = Users(data['id'], data['first_name'], data['last_name'], data['email'], data['bio'],
+                     data['username'], data['password'], data['image'], data['date'], data['active'])
+
+        categories = Categories(data['id'], data['category'])
+
+        post.user = user.__dict__
+
+        post.categories = categories.__dict__
 
         return post.__dict__
